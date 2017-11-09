cc5849adf0646754ccd7d3efab6a2a8540481e66
mayeen <km.shahriar28@gmail.com>
2017-11-09 17:13:08 +0600

    in mVC format, all works fine

---
+++ NeurotechDemo/Controller/IdentificationByScanner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neurotec.Licensing;
+using Neurotec.Biometrics.Client;
+using Neurotec.Biometrics;
+using System.IO;
+using Neurotec.Biometrics.Standards;
+using Neurotec.IO;
+using System.Windows.Forms;
+using NeurotechDemo.Model;
+
+
+namespace NeurotechDemo.Controller
+{
+    class IdentificationByScanner
+    {
+        public IdentificationByScanner(string subjectID)
+        {
+
+        }
+    }
+}
